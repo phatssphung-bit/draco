39a4fabb2d67b34b7a969bf3acf63ea9f71b92b1
phatssphung-bit <phatssphung@gmail.com>
2026-09-03 09:48:32 +0700

    Update Blaze Embers.lua

--- Blaze Embers.lua
+++ Blaze Embers.lua
@@ -1,9 +1,7 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Key = "31d4bebb966b95e8bd94d7a6"
 getgenv().Config = {
     ["Auto Quest Dojo Trainer"] = false,
     ["Auto Quest Dragon Hunter"] = true,
     ["Bring Mob Count"] = 6,
 }
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/Lotus_BF_Main.lua"))()
